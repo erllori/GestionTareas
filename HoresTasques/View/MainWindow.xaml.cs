6739e27cb19d46d5338376d1293194316aed976a
Marti Feina <mcotonat@asartec.com>
2025-05-29 16:09:58 +0200

    tornem a començar

--- HoresTasques/View/MainWindow.xaml.cs
+++ HoresTasques/View/MainWindow.xaml.cs
@@ -24,48 +24,5 @@ namespace HoresTasques
         {
             InitializeComponent();
         }
-
-        private void btnAfegir_Click(object sender, RoutedEventArgs e)
-        {
-            clsTasca novaTasca = new clsTasca();
-            TascaWindow frm = new TascaWindow(novaTasca);
-            frm.EsNova = true;
-
-            if (frm.ShowDialog() == true)
-            {
-                tasques.Add(novaTasca);
-                Utils.GuardarTasques(tasques.ToList(), CSVFilePath);
-            }
-        }
-
-        private void LlistaOrdenada_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (LlistaOrdenada.SelectedItem is clsTasca tascaSeleccionada)
-            {
-                TascaWindow frm = new TascaWindow(tascaSeleccionada);
-                frm.EsNova = false;
-                frm.ShowDialog();
-
-                if (tascaSeleccionada.Eliminada)
-                {
-                    tasques.Remove(tascaSeleccionada);
-                    Utils.GuardarTasques(tasques.ToList(), CSVFilePath);
-                }
-                else
-                {
-                    var sortedList = tasques.OrderBy(t => t.Data)
-                                    .ThenBy(t => !t.Finalitzada)
-                                    .ThenBy(t => t.Nom)
-                                    .ToList();
-                    tasques.Clear();
-                    foreach (var item in sortedList)
-                    {
-                        tasques.Add(item);
-                    }
-
-                    Utils.GuardarTasques(tasques.ToList(), CSVFilePath);
-                }
-            }
-        }
     }
 }
---
+++ HoresTasques/ViewModel/LlistaTascaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using HoresTasques.Model;
+using HoresTasques.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HoresTasques.ViewModel
+{
+    internal class LlistaTascaViewModel : BaseViewModel
+    {
+        
+    }
+}
